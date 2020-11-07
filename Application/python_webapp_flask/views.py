1b1a813f0317a0dfce9f1d623f654f3ca953aa79
Cloud User <kurihara@kuriCentOS8Python3.e45niof51yxu3hlw0rbqexavuf.lx.internal.cloudapp.net>
2020-11-07 13:01:43 +0000

    first commit

--- Application/python_webapp_flask/views.py
+++ Application/python_webapp_flask/views.py
@@ -3,35 +3,33 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import request, abort
 from python_webapp_flask import app
+import os
+
+from linebot import(
+    LineBotApi, WebhookHandler
+)
+
+from linebot.exceptions import(
+    InvalidSignatureError
+)
+
+from linebot.models import(
+    MessageEvent, TextMessage, TextSendMessage
+)
+
+#環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 @app.route('/')
-@app.route('/home')
-def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
+def hello_world():
+   return "hellow world!"
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = 
